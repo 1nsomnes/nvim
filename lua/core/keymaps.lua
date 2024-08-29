f52564df28266f790f3fc603ac11e292dc31ede0
Cedric <claecedric@gmail.com>
2024-08-29 13:42:57 -0700

    add telescope

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -21,3 +21,10 @@ keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 -- nvim tree
 keymap.set("n", "<leader>ee", ":NvimTreeToggle<CR>")   
 keymap.set("n", "<leader>ef", ":NvimTreeFocus<CR>")
+
+-- telescope 
+vim.keymap.set('n', '<leader>ff', "<cmd>Telescope find_files<CR>")
+vim.keymap.set('n', '<leader>fg', "<cmd>Telescope live_grep<CR>")
+vim.keymap.set('n', '<leader>fb', "<cmd>Telescope buffers<CR>") 
+vim.keymap.set('n', '<leader>fh', "<cmd>Telescope help_tags<cr>") 
+ 
