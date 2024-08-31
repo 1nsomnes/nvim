ea460827151eb1501a4ce146a63ec438bc5cfacb
Cedric <claecedric@gmail.com>
2024-08-31 02:01:25 -0700

    not working autocomplete

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,30 +1,38 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        after = "mason.nvim",  -- Ensure this runs after mason.nvim is loaded
-        config = function()
-            require("mason-lspconfig").setup({
-              ensure_installed= { "lua_ls", "pyright" }
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        after = "mason-lspconfig.nvim",  -- Ensure this runs after mason-lspconfig.nvim is loaded
-        config = function()
-            -- Example of setting up an LSP with mason
-            local lspconfig = require("lspconfig")
+  -- autocomplete stuff 
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-path',
+  'hrsh7th/cmp-cmdline',
+  'hrsh7th/nvim-cmp',
+  
+  -- vsnip
+  'hrsh7th/cmp-vsnip',
+  'hrsh7th/vim-vsnip',
 
-            -- Set up your LSP servers here
-            lspconfig.lua_ls.setup{}
-            lspconfig.pyright.setup{}
-        end,
+  config = function()
+    local cmp = require("cmp")
+
+    cmp.setup({
+      snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      end,
     },
+      mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+      }, {
+        { name = "buffer" },
+      })
+
+    })
+  end
 }
-
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,34 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        after = "mason.nvim",  -- Ensure this runs after mason.nvim is loaded
+        config = function()
+            require("mason-lspconfig").setup({
+              ensure_installed= { "lua_ls", "pyright" }
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        after =  { "mason-lspconfig.nvim", "cmp_nvim_lsp"},  -- Ensure this runs after mason-lspconfig.nvim is loaded
+        config = function()
+            -- Example of setting up an LSP with mason
+            local lspconfig = require("lspconfig")
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            -- Set up your LSP servers here
+            lspconfig.lua_ls.setup{
+              capabilities=capabilities
+            }
+            lspconfig.pyright.setup{
+              capabilities=capabilities
+            }
+        end,
+    },
+}
+
