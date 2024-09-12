61925460acb3b927d3c6efd65ad880c33c1ee692
Cedric <claecedric@gmail.com>
2024-09-12 10:13:20 -0700

    update tab and windows keybinds

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -13,10 +13,16 @@ keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
 keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
 
-keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
-keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
-keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
-keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab 
+-- Move between windows using Ctrl + h/j/k/l on macOS
+vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true, silent = true }) -- Move to the window on the left
+vim.keymap.set('n', '<C-j>', '<C-w>j', { noremap = true, silent = true }) -- Move to the window below
+vim.keymap.set('n', '<C-k>', '<C-w>k', { noremap = true, silent = true }) -- Move to the window above
+vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true, silent = true }) -- Move to the window on the right
+
+-- keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+keymap.set("n", "<leader>tx", "<cmd>BufferClose<CR>") -- close current tab
+keymap.set("n", "<leader>tn", "<cmd>BufferNext<CR>") --  go to next tab
+keymap.set("n", "<leader>tp", "<cmd>BufferPrevious<CR>") --  go to previous tab 
 
 -- nvim tree
 keymap.set("n", "<leader>ee", ":NvimTreeToggle<CR>")   
@@ -25,6 +31,5 @@ keymap.set("n", "<leader>ef", ":NvimTreeFocus<CR>")
 -- telescope 
 vim.keymap.set('n', '<leader>ff', "<cmd>Telescope find_files<CR>")
 vim.keymap.set('n', '<leader>fg', "<cmd>Telescope live_grep<CR>")
-vim.keymap.set('n', '<leader>fb', "<cmd>Telescope buffers<CR>") 
-vim.keymap.set('n', '<leader>fh', "<cmd>Telescope help_tags<cr>") 
- 
+vim.keymap.set('n', '<leader>fb', "<cmd>Telescope buffers<CR>")
+vim.keymap.set('n', '<leader>fh', "<cmd>Telescope help_tags<cr>")
