efc967968bd8471bfc3f689ec37365afeaa9c979
Cedric <claecedric@gmail.com>
2024-09-20 12:18:46 -0700

    tree sitter config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -38,6 +38,27 @@ return {
     },
     {
       "nvim-treesitter/nvim-treesitter",
+      event = { "BufReadPre", "BufNewFile" },
+      build = ":TSUpdate",
+      config = function()
+        local treesitter = require("nvim-treesitter.configs")
+
+        treesitter.setup({
+          highlight = {
+            enable = true,
+          },
+          indent = { enable = true },
+
+          ensure_installed = {
+            "json",
+            "lua",
+            "javascript",
+            "go",
+            "python",
+            "rust",
+          },
+        })
+      end,
     }
 }
 
